2e4398b8a45345606850434f7de764c7b00d3a31
Dmitry Dorofeev <dima@yasp.com>
2016-05-23 17:29:38 +0300

    First working version, which is not yet ready for public consumption. TODO: podspec and better error handling

---
+++ YASPWKTParser/Classes/YASPWKTParser.h
@@ -0,0 +1,13 @@
+//
+//  YASPWKTParser.h
+//  Pods
+//
+//  Created by Дмитрий Дорофеев on 22/05/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YASPWKTParser : NSObject
+
+@end
---
+++ YASPWKTParser/Classes/YASPWKTParser.m
@@ -0,0 +1,13 @@
+//
+//  YASPWKTParser.m
+//  Pods
+//
+//  Created by Дмитрий Дорофеев on 22/05/16.
+//
+//
+
+#import "YASPWKTParser.h"
+
+@implementation YASPWKTParser
+
+@end
